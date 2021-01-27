dac031589f33a3575092555728cfd61c05ad53ca
Aamir <aamir.talrop@gmail.com>
2021-01-27 17:29:48 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/screens/includes/Nav';
+import Main from './components/screens/Main';
+import Place from './components/screens/Place';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <>
+        <section className='wrapper'>
+              <Nav />
+              <Switch>
+                  <Route path='/' exact component={Main} />
+                  <Route path="/place/:id" component={Place} />
+              </Switch>
+        </section>
+      </>
+    </Router>
   );
 }
 
